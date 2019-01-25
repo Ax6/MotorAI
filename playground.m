4a02544eab80e5f8a4de525783e30638b71aca93
Aaron Russo <aaronrusso@hotmail.it>
2019-01-25 11:30:58 +0100

    Playground 1.0

--- playground.m
+++ playground.m
@@ -1,10 +1,15 @@
-for i=1:400
-    motorSimulation.setParameters(sim_parameters_(i, :));
-    y = motorSimulation.run();
-    figure(1)
-    hold on
-    plot(SIM_TIME, y(:, 1));
-    figure(2)
-    hold on
-    plot(SIM_TIME, y(:, 2));
+net_files = dir("net_*");
+
+Motors.f(:) = 5e-3;
+
+for i=1:length(net_files)
+    name = net_files(i).name;
+    file = load(name);
+    net = file.net;
+    try
+        [p_total, p_params, p_error, p_motors] = performance(net, Motors, motorParameters, motorSimulation);
+        disp(strcat(name, ": ", num2str(p_total)));
+    catch
+        warning(strcat(name, ": ", "Cazzo nope"));
+    end
 end
