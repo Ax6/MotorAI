72f8e5c6540cc68fb5b7a45601ee5474abc79702
Aaron Russo <aaronrusso@hotmail.it>
2019-01-27 23:13:27 +0100

    Added saturation protection to log normalisation

--- Functions/logDecode.m
+++ Functions/logDecode.m
@@ -1,5 +1,16 @@
 function decoded = logDecode(parameter_range, value)
-    max = log10(parameter_range(:, 2));
-    min = log10(parameter_range(:, 1));
+    max_ = parameter_range(:, 2);
+    min_ = parameter_range(:, 1);
+    max = log10(max_);
+    min = log10(min_);
+    
     decoded = 10.^((value .* (max - min)) + min);
+    
+    gcond = decoded > max_;
+    scond = decoded < min_;
+    if any(gcond)
+        decoded(gcond) = max_(gcond);
+    elseif any(scond)
+        decoded(scond) = min_(scond);
+    end
 end
--- Functions/logEncode.m
+++ Functions/logEncode.m
@@ -1,5 +1,16 @@
 function encoded = logEncode(parameter_range, value)
-    max = log10(parameter_range(:, 2));
-    min = log10(parameter_range(:, 1));
+    max_ = parameter_range(:, 2);
+    min_ = parameter_range(:, 1);
+    max = log10(max_);
+    min = log10(min_);
+    
+    gcond = value > max_;
+    scond = value < min_;
+    if any(gcond)
+        value(gcond) = max_(gcond);
+    elseif any(scond)
+        value(scond) = min_(scond);
+    end
+    
     encoded = (log10(value) - min) ./ (max - min);
 end
